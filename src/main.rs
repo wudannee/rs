68212fb3010d040245fe2fe225bfa661ef01cba9
wudannee <>
2024-11-15 08:58:26 +0800

    read name from stdin

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 fn main() {
-    let mut name = "rust";
+    let mut name = "rust".to_string();
     println!("i'm {}", name);
 
-    name = "wudannee";
+    println!("please input your name");
+    name.clear();
+    std::io::stdin()
+        .read_line(&mut name)
+        .expect("failed to read line");
+    name = name.trim().to_string();
     println!("i'm {}", name);
 }
